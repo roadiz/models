9b397c1870706bfdc75758c45ee19c854a5a7726
Ambroise Maupate <ambroise@rezo-zero.com>
2022-06-29 18:14:44 +0200

    fix: TranslationInterface must extend PersistableInterface

--- src/Roadiz/Core/AbstractEntities/TranslationInterface.php
+++ src/Roadiz/Core/AbstractEntities/TranslationInterface.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace RZ\Roadiz\Core\AbstractEntities;
 
-interface TranslationInterface
+interface TranslationInterface extends PersistableInterface
 {
     /**
      * @return string
