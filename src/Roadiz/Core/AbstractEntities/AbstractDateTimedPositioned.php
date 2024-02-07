3ab0957bf148175ae8883a55c0e18a887f604063
roadiz-ci <ambroise+ci@roadiz.io>
2024-02-07 13:42:57 +0000

    chore: Bumped

--- src/Roadiz/Core/AbstractEntities/AbstractDateTimedPositioned.php
+++ src/Roadiz/Core/AbstractEntities/AbstractDateTimedPositioned.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace RZ\Roadiz\Core\AbstractEntities;
 
-use ApiPlatform\Doctrine\Orm\Filter\NumericFilter;
-use ApiPlatform\Metadata\ApiFilter;
 use Doctrine\Common\Comparable;
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as Serializer;
 use Symfony\Component\Serializer\Annotation as SymfonySerializer;
-use ApiPlatform\Doctrine\Orm\Filter\RangeFilter;
 
 /**
  * Combined AbstractDateTimed and PositionedTrait.
@@ -31,9 +28,7 @@ abstract class AbstractDateTimedPositioned extends AbstractDateTimed implements 
         ORM\Column(type: "float"),
         Serializer\Groups(["position"]),
         Serializer\Type("float"),
-        SymfonySerializer\Groups(["position"]),
-        ApiFilter(RangeFilter::class),
-        ApiFilter(NumericFilter::class)
+        SymfonySerializer\Groups(["position"])
     ]
     protected float $position = 0.0;
 }
