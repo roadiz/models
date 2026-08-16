bf2ed32cb37b086b7cc998a9fa27744ed6e3f565
roadiz-ci <ambroise+ci@roadiz.io>
2026-08-16 13:40:43 +0000

    chore: bumped

--- src/Core/AbstractEntities/AbstractEntity.php
+++ src/Core/AbstractEntities/AbstractEntity.php
@@ -11,40 +11,27 @@ use Symfony\Component\Serializer\Annotation as SymfonySerializer;
 /**
  * Base entity implementing PersistableInterface to offer a unique ID.
  */
-#[
-    ORM\MappedSuperclass,
-    ORM\Table
-]
+#[ORM\MappedSuperclass,
+    ORM\Table]
 abstract class AbstractEntity implements PersistableInterface
 {
-    /**
-     * @var int|string|null
-     */
-    #[
-        ORM\Id,
-        ORM\Column(type: "integer"),
+    #[ORM\Id,
+        ORM\Column(type: 'integer'),
         ORM\GeneratedValue,
-        Serializer\Groups(["id"]),
-        Serializer\Type("integer"),
-        SymfonySerializer\Groups(["id"])
-    ]
+        Serializer\Groups(['id']),
+        Serializer\Type('integer'),
+        SymfonySerializer\Groups(['id'])]
     protected int|string|null $id = null;
 
-    /**
-     * @return int|string|null
-     */
     public function getId(): int|string|null
     {
         return $this->id;
     }
 
-    /**
-     * @param int|string|null $id
-     * @return AbstractEntity
-     */
     public function setId(int|string|null $id): self
     {
         $this->id = $id;
+
         return $this;
     }
 }
